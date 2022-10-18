9c71598fa5528ea509d4c0403edba8fad3f90108
Bruce <39899119+792607724@users.noreply.github.com>
2022-10-18 16:28:33 +0800

    2
    2

--- SeewoTestTool/SeewoTestTool/Form1.cs
+++ SeewoTestTool/SeewoTestTool/Form1.cs
@@ -17,18 +17,30 @@ namespace SeewoTestTool
 
         private void device_connect_button_Click(object sender, EventArgs e)
         {
-            string ip_address = device_ip_textbox.Text;
+            string host = device_ip_textbox.Text;
             string port = device_port_textbox.Text;
-            if (string.IsNullOrEmpty(ip_address) || string.IsNullOrEmpty(port))
+            if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(port))
             {
                 MessageBox.Show("设备网口IP地址和端口号不能为空！");
             }
             else
             {
+                // Socket Connection Build
                 int port_int = Convert.ToInt32(port);
-                MessageBox.Show($"IP：{ip_address} PORT：{port_int}");
+                MessageBox.Show($"IP：{host} PORT：{port_int}");
+                IPAddress ip = IPAddress.Parse(host);
+                IPEndPoint ipe = new IPEndPoint(ip, port_int);
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(ipe);
 
             }
         }
+
+        private void send_command(Socket clientSocket, string command)
+        { 
+
+        }
+
+
     }
 }
